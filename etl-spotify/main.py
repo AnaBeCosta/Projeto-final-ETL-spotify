b386cc7f5e261421e5efddae72c392360c4cd8fc
isabelamenezs <isabelaclmenezes1@gmail.com>
2025-11-21 18:20:18 -0300

    Transformação da coluna track_duration_ms de milisegundos para minutos

--- etl-spotify/main.py
+++ etl-spotify/main.py
@@ -4,13 +4,23 @@ import pandas as pd
 import numpy as np
 
 
-df1_csv = pd.read_csv('spotify_data clean.csv')
-df2_csv = pd.read_csv('track_data_final.csv')
+df1_csv = pd.read_csv('etl-spotify/spotify_data_clean.csv')
+df2_csv = pd.read_csv('etl-spotify/track_data_final.csv')
 
 #print(df1_csv.info())
 #print(df2_csv.info())
 
-print(df1_csv.isna().sum())
-print(df2_csv.isna().sum())
+
 
 ##TRANSFORMAÇÃO
+
+#Tranformação da coluna track_duration_ms de milisegundos para minutos
+df2_csv['track_duration_min'] = df2_csv['track_duration_ms'] / 60000
+df2_csv['track_duration_min'] = df2_csv['track_duration_min'].round(2)
+
+#print(df2_csv.columns)
+
+#Remoção da coluna track_duration_ms
+df2_csv = df2_csv.drop(columns=['track_duration_ms'])
+
+#print(df2_csv.info())
