9599c81101af6e74779cf8de0dfda4d546a1a816
Ana Beatriz Costa <66637302+AnaBeCosta@users.noreply.github.com>
2025-11-21 19:00:36 -0300

    Ajuste

--- etl-spotify/main.py
+++ etl-spotify/main.py
@@ -4,30 +4,23 @@ print("******Iniciando o processo de ETL para dados do Spotify...******\n")
 import pandas as pd
 import numpy as np
 
-print("******Iniciando o processo de ETL para dados do Spotify...******\n")
-
-
 df1_csv = pd.read_csv('spotify_data_clean.csv')
 df2_csv = pd.read_csv('track_data_final.csv')
-
 print("******Finalizando o processo de ETL******\n")
 
+
+
 ##TRANSFORMAÇÃO
+print("******Iniciando o processo de transformação******\n")
 
 #Tranformação da coluna track_duration_ms de milisegundos para minutos
 df2_csv['track_duration_min'] = df2_csv['track_duration_ms'] / 60000
 df2_csv['track_duration_min'] = df2_csv['track_duration_min'].round(2)
 
-#print(df2_csv.columns)
-
 #Remoção da coluna track_duration_ms
 df2_csv = df2_csv.drop(columns=['track_duration_ms'])
 
-#print(df2_csv.info())
-
-print("******Iniciando o processo de transformação******\n")
-
-print('- Checagem de dados duplicados')
+#Remoção dos duplicados
 duplicados_df1 = df1_csv.duplicated().sum()
 duplicados_df2 = df2_csv.duplicated().sum()
 
@@ -36,8 +29,7 @@ if (duplicados_df1 > 0 or duplicados_df2 > 0) :
     df2_csv = df2_csv.drop_duplicates()
     print('Linhas duplicadas removidas com sucesso!')
 
-
-print('- Tratamento de valores nulos')
+#Tratamento dos nulos
 df1_csv.dropna(subset=['artist_followers'], inplace=True)
 
 colunas1 = ['artist_genres', 'artist_name']
@@ -46,11 +38,7 @@ colunas2 = ['track_name', 'artist_genres', 'album_name', 'artist_name']
 df1_csv[colunas1] = df1_csv[colunas1].fillna('Não informado')
 df2_csv[colunas2] = df2_csv[colunas2].fillna('Não informado')
 
-print('- Tratamento de valores nulos')
-
 ##Convertendo formato de data (MM/DD/AA → DD/MM/AA)'
-print('- Tratamento da formatação de data')
-
 colunas3 = ['album_release_date']
 
 df1_csv[colunas3] = pd.to_datetime(df1_csv[colunas3], format='%m/%d/%y', errors='coerce')
@@ -58,4 +46,3 @@ df2_csv[colunas3] = pd.to_datetime(df2_csv[colunas3], format='%m/%d/%y', errors=
 
 df1_csv[colunas3] = df1_csv[colunas3].dt.strftime('%d/%m/%y')
 df2_csv[colunas3] = df2_csv[colunas3].dt.strftime('%d/%m/%y')
-
